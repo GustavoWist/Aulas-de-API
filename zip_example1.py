66567134d77752627b9d30b491432f1df5fa8abe
Gustavo Wistefelt <Gustavowistefelt.lk@gamil.com>
2024-08-29 18:39:15 -0300

    Aulas de API backup

---
+++ zip_example1.py
@@ -0,0 +1,15 @@
+estudantes = ['Gustavo','Beatriz', 'Giane', 'Wusleid', 'Mafalda']
+matematica = [7.0,7.0,7.0,7.0,7.0]
+portugues = [8.0,8.0,8.0,8.0,8.0]
+historia = [9.0,9.0,9.0,9.0,9.0]
+
+resultado = [
+    {
+        'Nome': estudante,
+        'Matematica': nota_matematica,
+        'Portugues': nota_portugues,
+        'Historia': nota_historia
+    }
+    for estudante, nota_matematica,nota_portugues,nota_historia in zip(estudantes, matematica,portugues,historia)
+]
+print(resultado)
